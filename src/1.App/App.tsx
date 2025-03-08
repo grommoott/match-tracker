604735ab3a911664afc18f9b74227c897b647b22
grommoott <grommoott@gmail.com>
2025-03-08 21:56:38 +0700

    add: <MainPage/>

--- src/1.App/App.tsx
+++ src/1.App/App.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react"
 import "./styles/global.scss"
+import { MainPage } from "@pages/MainPage"
 
 export const App: FC = () => {
-    return <div></div>
+    return <MainPage />
 }
---
+++ src/2.Pages/MainPage/MainPage.tsx
@@ -0,0 +1,8 @@
+import { MatchListWidget } from "@widgets/MatchListWidget"
+import { FC } from "react"
+
+interface Props {}
+
+export const MainPage: FC<Props> = () => {
+    return <MatchListWidget />
+}
