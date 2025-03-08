59ba886bbf019dde65450ee3066b801e1346df56
grommoott <grommoott@gmail.com>
2025-03-08 21:46:30 +0700

    add: isLoading to <RefreshButton/>

--- src/5.Shared/ui/RefreshButton/RefreshButton.tsx
+++ src/5.Shared/ui/RefreshButton/RefreshButton.tsx
@@ -4,13 +4,20 @@ import refreshImage from "/refresh.svg"
 
 interface Props {
     onClick?: () => void
+    isLoading?: boolean
 }
 
-export const RefreshButton: FC<Props> = ({ onClick = () => {} }) => {
+export const RefreshButton: FC<Props> = ({
+    onClick = () => {},
+    isLoading = false,
+}) => {
     return (
         <button className={style.refreshButton} onClick={onClick}>
             <p>Обновить</p>
-            <img src={refreshImage} className={style.refreshImage} />
+            <img
+                src={refreshImage}
+                className={`${style.refreshImage} ${isLoading && style.spinning}`}
+            />
         </button>
     )
 }
