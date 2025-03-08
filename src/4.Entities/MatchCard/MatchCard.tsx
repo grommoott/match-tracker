f1f0cfbbd7f123e768325f8edebfee9c160f173a
grommoott <grommoott@gmail.com>
2025-03-08 20:46:17 +0700

    add: <MatchCard/>

---
+++ src/4.Entities/MatchCard/MatchCard.tsx
@@ -0,0 +1,32 @@
+import { Match } from "@types"
+import { FC } from "react"
+import style from "./styles/matchCard.module.scss"
+import teamIcon from "/team-icon.svg"
+import { CardStatus } from "@ui/CardStatus"
+
+interface Props {
+    match: Match
+}
+
+export const MatchCard: FC<Props> = ({ match }) => {
+    return (
+        <div className={style.matchCard}>
+            <div className={style.teamContainer}>
+                <img src={teamIcon} />
+                <p>{match.homeTeam.name}</p>
+            </div>
+
+            <div className={style.scoreContainer}>
+                <p>
+                    {match.homeScore} : {match.awayScore}
+                </p>
+                <CardStatus status={match.status} />
+            </div>
+
+            <div className={style.teamContainer}>
+                <p>{match.awayTeam.name}</p>
+                <img src={teamIcon} />
+            </div>
+        </div>
+    )
+}
