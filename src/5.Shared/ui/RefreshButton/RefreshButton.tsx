dddd8e026d75505e6f9ffe2f71029db8e019e275
grommoott <grommoott@gmail.com>
2025-03-08 17:34:02 +0700

    add: <RefreshButton/>

---
+++ src/5.Shared/ui/RefreshButton/RefreshButton.tsx
@@ -0,0 +1,16 @@
+import { FC } from "react"
+import style from "./styles/refreshButton.module.scss"
+import refreshImage from "/refresh.svg"
+
+interface Props {
+    onClick?: () => void
+}
+
+export const RefreshButton: FC<Props> = ({ onClick = () => {} }) => {
+    return (
+        <button className={style.refreshButton} onClick={onClick}>
+            <p>Обновить</p>
+            <img src={refreshImage} className={style.refreshImage} />
+        </button>
+    )
+}
