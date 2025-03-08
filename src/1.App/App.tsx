186adef7f1dc2fe97bdc1af31a2406c2e0314476
grommoott <grommoott@gmail.com>
2025-03-08 17:09:56 +0700

    add: init and FSD structure

---
+++ src/1.App/App.tsx
@@ -0,0 +1,6 @@
+import { FC } from "react"
+import "./styles/global.scss"
+
+export const App: FC = () => {
+    return <div></div>
+}
---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+
+console.log(process.cwd())
+
+// https://vite.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    resolve: {
+        alias: {
+            "@app": process.cwd() + "/src/1.App",
+            "@pages": process.cwd() + "/src/2.Pages",
+            "@widgets": process.cwd() + "/src/3.Widgets",
+            "@entities": process.cwd() + "/src/4.entities",
+            "@shared": process.cwd() + "/src/5.Shared",
+        },
+    },
+})
